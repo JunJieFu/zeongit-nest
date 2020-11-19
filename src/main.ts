fb52fb8aeb447736c8119369d641b8ebc4199458
Zeongit <245142637@qq.com>
2020-11-19 10:54:55 +0800

    统一处理

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import { AllExceptionsFilter } from "./share/filter/http_exception.filter"
+import { AdviceInterceptor } from "./share/interceptor/advice.interceptor"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalFilters(new AllExceptionsFilter())
+  app.useGlobalInterceptors(new AdviceInterceptor())
   await app.listen(9000)
 }
+
 bootstrap()
