fd10dd56e32af6a75a43527f4cc95d69126d4ce9
Zeongit <245142637@qq.com>
2020-11-26 14:34:40 +0800

    修改

--- src/web/controller/user-info.controller.ts
+++ src/web/controller/user-info.controller.ts
@@ -1,10 +1,42 @@
-import { Controller, Get } from "@nestjs/common"
-import { plainToClass } from "class-transformer"
+import { Body, Controller, Get } from "@nestjs/common"
+import { Type } from "class-transformer"
 import { JwtAuth } from "../../auth/decorator/jwt-auth.decorator"
 import { UserInfoEntity } from "../../data/entity/user-info.entity"
 import { CurrentUser } from "../../auth/decorator/current-user.decorator"
-import { UserInfoVo } from "../vo/user-info.vo"
 import { UserInfoService } from "../service/user-info.service"
+import { Gender } from "../../data/constant/gender.constant"
+import { IsDate, IsEnum, IsOptional, IsString } from "class-validator"
+
+class UserInfoDto {
+  @IsEnum(Gender)
+  @IsOptional()
+  gender?: Gender
+
+  @Type(() => Date)
+  @IsDate()
+  @IsOptional()
+  birthday?: Date
+
+  @IsString()
+  @IsOptional()
+  nickname?: string
+
+  @IsString()
+  @IsOptional()
+  introduction?: string
+
+  @IsString()
+  @IsOptional()
+  country?: string
+
+  @IsString()
+  @IsOptional()
+  province?: string
+
+  @IsString()
+  @IsOptional()
+  city?: string
+}
 
 
 @Controller("userInfo")
@@ -16,13 +48,36 @@ export class UserInfoController {
   @JwtAuth()
   @Get("get")
   get(@CurrentUser() userInfo: UserInfoEntity) {
-    return plainToClass(UserInfoVo, userInfo, { excludeExtraneousValues: true })
+    return userInfo
   }
 
-  // @JwtAuth()
-  // @Post("getByPhone")
-  // getByPhone():
-  //   NestResponse<any> {
-  //   return this.userService.getByPhone("123")
-  // }
+  @JwtAuth()
+  @Get("save")
+  save(@CurrentUser() userInfo: UserInfoEntity,
+       @Body(){
+         gender,
+         birthday,
+         nickname,
+         introduction,
+         country,
+         province,
+         city
+       }: UserInfoDto) {
+    userInfo.gender = gender ?? userInfo.gender
+    userInfo.birthday = birthday ?? userInfo.birthday
+    userInfo.nickname = nickname ?? userInfo.nickname
+    userInfo.introduction = introduction ?? userInfo.introduction
+    userInfo.country = country ?? userInfo.country
+    userInfo.province = province ?? userInfo.province
+    userInfo.city = city ?? userInfo.city
+
+    return this.userInfoService.save(userInfo)
+  }
+
+// @JwtAuth()
+// @Post("getByPhone")
+// getByPhone():
+//   NestResponse<any> {
+//   return this.userService.getByPhone("123")
+// }
 }
--- src/web/service/user-info.service.ts
+++ src/web/service/user-info.service.ts
@@ -2,13 +2,19 @@ import { Injectable } from "@nestjs/common"
 import { Repository } from "typeorm"
 import { InjectRepository } from "@nestjs/typeorm"
 import { UserInfoEntity } from "../../data/entity/user-info.entity"
+import { UserInfoCache } from "../../data/cache/user-info.cache"
 
 
 @Injectable()
 export class UserInfoService {
   constructor(
     @InjectRepository(UserInfoEntity)
-    private readonly userRepository: Repository<UserInfoEntity>
+    private readonly userInfoRepository: Repository<UserInfoEntity>,
+    private readonly userInfoCache: UserInfoCache
   ) {
+  }
+
+  save(userInfo: UserInfoEntity) {
+    return this.userInfoCache.save(userInfo)
   }
 }
