c8ef05d2a3e5626d54b28f38eca79ea605010a07
Zeongit <245142637@qq.com>
2020-12-10 14:38:02 +0800

    使用缓存配置

--- src/data/config-service/cache-config.service.ts
+++ src/data/config-service/cache-config.service.ts
@@ -1,14 +1,21 @@
-import { CacheModuleOptions, CacheOptionsFactory, Injectable } from "@nestjs/common"
+import { CacheModuleOptions, CacheOptionsFactory, Inject, Injectable } from "@nestjs/common"
+import { cacheConfigType } from "../config"
+import { ConfigType } from "@nestjs/config"
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const redisStore = require("cache-manager-redis-store")
 
 @Injectable()
 export class CacheConfigService implements CacheOptionsFactory {
+  constructor(@Inject(cacheConfigType.KEY)
+              private cacheConfig: ConfigType<typeof cacheConfigType>
+  ) {
+  }
+
   createCacheOptions(): CacheModuleOptions {
     return {
       store: redisStore,
-      host: "localhost",
-      port: 6379,
+      host: this.cacheConfig.host,
+      port: this.cacheConfig.port,
       ttl: 100
     }
   }
