43031932708d3a51b1aee83c5bfac8f3a4b62661
Zeongit <245142637@qq.com>
2021-02-23 23:53:12 +0800

    emoji

--- src/share/uitl/emoji.util.ts
+++ src/share/uitl/emoji.util.ts
@@ -1,3 +1,3 @@
 export function emojiChange(value:string){
-  return value.replace(/\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDE4F]/g, "");
+  return value.replace(/\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDE4F]/g, "").replace(/[^\\x00-xF]/g,"");
 }
