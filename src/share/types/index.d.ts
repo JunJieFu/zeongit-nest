218653030fb8388a2f4aef4d8c21d649222f87da
Zeongit <245142637@qq.com>
2021-02-23 10:59:33 +0800

    包装分页

--- src/share/types/index.d.ts
+++ src/share/types/index.d.ts
@@ -3,3 +3,14 @@ import { Observable } from "rxjs"
 type NestResponse<T> = T | Promise<T> | Observable<T>
 
 type NodeEnv = "development" | "production"
+
+interface PaginationMeta{
+  itemCount: number;
+  totalItems: number;
+  itemsPerPage: number;
+  totalPages: number;
+  currentPage: number;
+  empty: boolean;
+  first: boolean;
+  last: boolean;
+}
