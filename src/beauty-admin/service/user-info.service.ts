32bc3c95d0e7fc5946f934aaba2383dbaa10828a
Zeongit <245142637@qq.com>
2021-04-08 11:23:43 +0800

    一个小bug

--- src/beauty-admin/service/user-info.service.ts
+++ src/beauty-admin/service/user-info.service.ts
@@ -3,19 +3,21 @@ import { Repository } from "typeorm"
 import { UserInfoEntity } from "../../data/entity/account/user-info.entity"
 import { nullable } from "../../share/fragment/pipe.function"
 import { InjectAccount } from "../../data/decorator/inject-account.decorator"
+import { UserInfoCache } from "../../data/cache/user-info.cache";
 
 @Injectable()
 export class UserInfoService {
   constructor(
     @InjectAccount(UserInfoEntity)
-    private readonly userInfoRepository: Repository<UserInfoEntity>) {
+    private readonly userInfoRepository: Repository<UserInfoEntity>,
+    private readonly userInfoCache: UserInfoCache) {
   }
 
   get(id: number) {
-    return this.userInfoRepository.findOne({ id }).then(nullable("用户不存在"))
+    return this.userInfoRepository.findOne({id}).then(nullable("用户不存在"))
   }
 
   save(userInfo: UserInfoEntity) {
-    return this.userInfoRepository.save(userInfo)
+    return this.userInfoCache.save(userInfo)
   }
 }
--- src/beauty-admin/service/user.service.ts
+++ src/beauty-admin/service/user.service.ts
@@ -5,26 +5,28 @@ import { UserEntity } from "../../data/entity/account/user.entity"
 import { UserInfoEntity } from "../../data/entity/account/user-info.entity"
 import { UserInfoService } from "./user-info.service"
 import { InjectAccount } from "../../data/decorator/inject-account.decorator"
+import { UserCache } from "../../data/cache/user.cache";
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectAccount(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
-    private readonly userInfoService: UserInfoService
+    private readonly userInfoService: UserInfoService,
+    private readonly userCache: UserCache
   ) {
   }
 
   get(id: number) {
-    return this.userRepository.findOne({ id }).then(nullable("用户不存在"))
+    return this.userRepository.findOne({id}).then(nullable("用户不存在"))
   }
 
   countByPhone(phone: string) {
-    return this.userRepository.count({ phone })
+    return this.userRepository.count({phone})
   }
 
   async signUp(phone: string, password: string) {
-    const count = await this.userRepository.count({ phone })
+    const count = await this.userRepository.count({phone})
     if (count) {
       throw new HttpException("手机号码已存在", HttpStatus.FORBIDDEN)
     }
@@ -35,6 +37,6 @@ export class UserService {
   }
 
   private save(user: UserEntity) {
-    return this.userRepository.save(user)
+    return this.userCache.save(user)
   }
 }
