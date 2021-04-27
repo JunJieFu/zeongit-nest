d9ecbd48360305a3bf641936a75200a005e37639
Zeongit <245142637@qq.com>
2021-04-27 11:44:20 +0800

    试试上传

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,9 @@
-import { Test, TestingModule } from "@nestjs/testing"
 import { INestApplication } from "@nestjs/common"
+import { Test, TestingModule } from "@nestjs/testing"
 import * as request from "supertest"
 import { AccountModule } from "../src/account/account.module"
 
-describe("AppController (e2e)", () => {
+describe("AppController (e2e)", () => { 
   let app: INestApplication
 
   beforeEach(async () => {
